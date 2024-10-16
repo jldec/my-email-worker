0deffcbd50a8877b313ca22a17a626310d496509
jldec <jurgen@jldec.me>
2024-10-16 16:27:52 +0100

    parse email attachments

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
+// MIT https://github.com/postalsys/postal-mime/blob/master/LICENSE.txt
+import PostalMime from 'postal-mime';
+
 export default {
-  async email(message: ForwardableEmailMessage, env: Env, ctx: ExecutionContext) {
-    if (message.from !== "jurgen@ciaosoft.com") {
-      console.log(`Reject email from ${message.from}`)
-      message.setReject(`Not allowed`);  
+  async email(
+    message: ForwardableEmailMessage,
+    env: Env,
+    ctx: ExecutionContext
+  ) {
+    if (message.from !== 'jurgen@ciaosoft.com') {
+      console.log(`Reject email from ${message.from}`);
+      message.setReject(`Not allowed`);
       return;
     }
-    console.log(`Forward email from ${message.from}`)
-    await message.forward("jurgen@jldec.me");
-  }
-}
+    console.log(`Received email from ${message.from}`);
+
+    // parse for attachments - see readme below for additional options
+    // https://github.com/postalsys/postal-mime/tree/master?tab=readme-ov-file#postalmimeparse
+    const email = await PostalMime.parse(message.raw);
+    email.attachments.forEach((a) => {
+      if (a.mimeType.startsWith('application/json')) {
+        const jsonString = new TextDecoder().decode(a.content);
+        const jsonValue = JSON.parse(jsonString);
+        console.log(
+          `JSON attachment value:\n${JSON.stringify(jsonValue, null, 2)}`
+        );
+      }
+    });
+
+    await message.forward('jurgen@haydnlabs.com');
+  },
+};
