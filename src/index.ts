7155997078c199b509ab2f5a519b1a151b4b1067
jldec <jurgen@jldec.me>
2024-10-17 18:27:47 +0100

    not works

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { EmailMessage } from 'cloudflare:email'
+import { createMimeMessage } from 'mimetext'
 import PostalMime from 'postal-mime'
 
 export default {
@@ -14,6 +16,27 @@ export default {
         console.log(`JSON attachment value:\n${JSON.stringify(jsonValue, null, 2)}`)
       }
     })
+
+    // reply to sender
+    // https://developers.cloudflare.com/email-routing/email-workers/reply-email-workers/
+    const messageId = message.headers.get('Message-ID')
+    if (messageId) {
+      console.log(`Replying to sender with message id:${messageId}`)
+      const msg = createMimeMessage()
+      msg.setHeader('In-Reply-To', messageId)
+      msg.setHeader('From', 'test-email-worker@jldec.fun')
+      msg.setSender('test-email-worker@jldec.me')
+      msg.setRecipient(message.from)
+      msg.setSubject('test-email-worker auto reply')
+      msg.addMessage({
+        contentType: 'text/plain',
+        data: `Thanks for your message!`
+      })
+
+      ctx.waitUntil(
+        message.reply(new EmailMessage('test-email-worker@jldec.fun', message.from, msg.asRaw()))
+      )
+    }
 
     ctx.waitUntil(message.forward('jurgen@haydnlabs.com'))
   }
